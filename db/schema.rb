1069ed88eeb4c3fd76ddcc12f0c3e5704b397469
Dale Watson <dwatson27@hotmail.com>
2018-07-10 11:54:41 +0000

    changed database to postgresql from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_21_023826) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
